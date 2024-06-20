2f7d4cd3a60faa1719b343bdf89f70302e41a915
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2024-06-21 01:52:29 +0300

    Browsing page done

--- app_client/src/components/Header/Header.tsx
+++ app_client/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import './Header.css'
-import logo from '../../images/logo.svg'
+import './Header.css';
+import logo from '../../images/logo.svg';
 import MenuItem from '../MenuItem/MenuItem';
 
 interface ButtonProps {
