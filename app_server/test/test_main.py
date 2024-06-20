b439b85f907021891ea324783bd8fe79883d8687
justcgh9 <a.kabardiyadi@innopolis.university>
2024-06-21 01:03:50 +0500

    Edited a test

--- app_server/test/test_main.py
+++ app_server/test/test_main.py
@@ -1,6 +1,22 @@
 import subprocess
 import pytest
+import re
+import ast
 
+def extract_dict_from_text(text):
+    dict_pattern = re.compile(r"\{.*?\}", re.DOTALL)
+
+    match = dict_pattern.search(text)
+    if match:
+        dict_str = match.group()
+        try:
+            dictionary = ast.literal_eval(dict_str)
+            return dictionary
+        except (SyntaxError, ValueError) as e:
+            print(f"Error evaluating the dictionary: {e}")
+    else:
+        print("No dictionary found in the text.")
+    return None
 
 def test_get_all():
 
@@ -22,4 +38,6 @@ def test_upload_file():
 
     result_text = result.stdout.split('\n')
     status = result_text[0].split(' ')[1]
+    response = extract_dict_from_text(result.stdout)
     assert status == '200'
+    assert len(response['tasks']) == 13
