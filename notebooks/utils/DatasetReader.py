216966e997aa5286ef7b618efa829af74fa4406b
Ruslan <r.izmajlov2018@yandex.ru>
2024-06-20 21:09:12 +0300

    feat: dataset reader encoding feature

--- notebooks/utils/DatasetReader.py
+++ notebooks/utils/DatasetReader.py
@@ -1,22 +1,27 @@
 import os
 import pandas as pd
 from string import punctuation
+from sklearn.preprocessing import LabelEncoder
 
 
 class DatasetReader:
-    def __init__(self):
-        pass
+    def __init__(self, encode_labels=False):
+        self.encode_labels = encode_labels
 
     def read(self, *filenames) -> pd.DataFrame:
-        data = [self.read_from_file(filename) for filename in filenames]
+        data = [self.read_file(filename) for filename in filenames]
         return pd.concat(data, ignore_index=True)
 
-    def read_from_file(self, filename) -> pd.DataFrame:
+    def read_file(self, filename) -> pd.DataFrame:
         data = pd.read_csv(filename)
         data = data[data['Topic'] != 'Unit2'][['Questions', 'Topic']]
 
-        data['Topic'] = data['Topic'].map(lambda x: '_'.join(x.split()))
         data['Questions'] = data['Questions'].map(str.lower)
+
+        if self.encode_labels:
+            data['Topic'] = data['Topic'].map(lambda x: '_'.join(x.split()))
+            data['Topic'] = LabelEncoder().fit_transform(data['Topic'])
+
         return data
 
     def read_from_dir(self, string):
@@ -24,7 +29,10 @@ class DatasetReader:
 
 
 if __name__ == '__main__':
-    data = DatasetReader().read('../../data/csv/clean_data.csv',
-                                '../../data/parsing/save_my_exams_data.csv')
+    data1 = DatasetReader(encode_labels=True).read('../../data/csv/clean_data.csv')
+    data2 = DatasetReader().read('../../data/csv/clean_data.csv')
+    data3 = DatasetReader(encode_labels=True).read_file('../../data/csv/clean_data.csv')
 
-    print(data)
+    print(data1[:4], end='\n\n')
+    print(data2[:4], end='\n\n')
+    print(data3[:4], end='\n\n')
