bb3abf157a3f7e65696796f26f940dca4ef4fac0
Ruslan <r.izmajlov2018@yandex.ru>
2024-06-20 23:08:00 +0300

    feat: dataset split inside reader feature

--- notebooks/utils/DatasetReader.py
+++ notebooks/utils/DatasetReader.py
@@ -2,17 +2,31 @@ import os
 import pandas as pd
 from string import punctuation
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 
 class DatasetReader:
-    def __init__(self, encode_labels=False):
+    def __init__(self, encode_labels=False, test_size=None):
         self.encode_labels = encode_labels
+        self.test_size = test_size
 
     def read(self, *filenames) -> pd.DataFrame:
-        data = [self.read_file(filename) for filename in filenames]
-        return pd.concat(data, ignore_index=True)
+        data = [self._read_file(filename) for filename in filenames]
+        data = pd.concat(data, ignore_index=True)
+
+        if self.test_size is not None:
+            return train_test_split(data, train_size=self.test_size)
+
+        return data
 
     def read_file(self, filename) -> pd.DataFrame:
+        data = self._read_file(filename)
+        if self.test_size is not None:
+            return train_test_split(data, train_size=self.test_size)
+
+        return data
+
+    def _read_file(self, filename) -> pd.DataFrame:
         data = pd.read_csv(filename)
         data = data[data['Topic'] != 'Unit2'][['Questions', 'Topic']]
 
@@ -24,15 +38,11 @@ class DatasetReader:
 
         return data
 
-    def read_from_dir(self, string):
+    def read_dir(self, string):
         raise NotImplemented()
 
 
 if __name__ == '__main__':
-    data1 = DatasetReader(encode_labels=True).read('../../data/csv/clean_data.csv')
-    data2 = DatasetReader().read('../../data/csv/clean_data.csv')
-    data3 = DatasetReader(encode_labels=True).read_file('../../data/csv/clean_data.csv')
+    data = DatasetReader(encode_labels=True).read('../../data/csv/clean_data.csv')
 
-    print(data1[:4], end='\n\n')
-    print(data2[:4], end='\n\n')
-    print(data3[:4], end='\n\n')
+    print(data.describe())
