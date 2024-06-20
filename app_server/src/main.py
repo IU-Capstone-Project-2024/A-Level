a7fd441e895cd3ba616dd13eca71381aa04115da
justcgh9 <a.kabardiyadi@innopolis.university>
2024-06-21 01:37:44 +0500

    Enhanced the API capabilities

--- app_server/src/main.py
+++ app_server/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import document
+from src.routers import document, task
 from src.storages.mongo import lifespan
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -8,12 +8,13 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace with your actual allowed origins
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],   # Replace with your allowed methods
-    allow_headers=["*"]    # Replace with your allowed headers
+    allow_methods=["*"], 
+    allow_headers=["*"] 
 )
 
 app.include_router(document.router)
+app.include_router(task.router)
 
 
