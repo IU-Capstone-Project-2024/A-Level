4168d7e43080ef9136b7e78983b82c02df17745f
justcgh9 <a.kabardiyadi@innopolis.university>
2024-06-22 13:05:44 +0500

    Added Naive Bayes Model Support

--- app_server/src/routers/task.py
+++ app_server/src/routers/task.py
@@ -1,10 +1,18 @@
-
+import logging
 import aiohttp
 from beanie import PydanticObjectId
 from fastapi import APIRouter, HTTPException
 
 from src.storages.mongo.models.task import Task, TaskCreate, TaskUpdate, Topic
 from src.storages.mongo.repositories.task import task_repository
+
+file_handler = logging.FileHandler('task-router.log')
+file_handler.setLevel(logging.INFO)  
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+
+logging.getLogger('').addHandler(file_handler)
 
 router = APIRouter(prefix="/task", tags=["Task"])
 
@@ -41,3 +49,14 @@ async def update(task_id: PydanticObjectId, task_update: TaskUpdate) -> Task | N
 async def delete(task_id: PydanticObjectId) -> Task | None:
     return await task_repository.delete(task_id)
 
+
+@router.get("/{task_id}/predict")
+async def predict(task_id: PydanticObjectId):
+    task = await task_repository.read(task_id)
+    if task is None:
+        return HTTPException(status_code=404, detail=f"Task {task_id} does not exists")
+
+    data = {"request": task.content}
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        async with session.post("http://model:8000/predict", json=data) as response:
+            return await response.json()
