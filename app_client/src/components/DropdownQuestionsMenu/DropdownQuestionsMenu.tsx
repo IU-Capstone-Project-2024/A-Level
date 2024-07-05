891baba119eb2d20e2ca0933e61d1e53d6ea4fda
spaghettikoder <a.meslmani@innopolis.university>
2024-07-05 03:21:52 +0300

    fixing syntax error

--- app_client/src/components/DropdownQuestionsMenu/DropdownQuestionsMenu.tsx
+++ app_client/src/components/DropdownQuestionsMenu/DropdownQuestionsMenu.tsx
@@ -1,63 +1,53 @@
-import selectedOptionIcon from '../../images/selectedOption.svg';
-import dropArrowIcon from '../../images/dropArrowIcon.svg';
+import selectedOptionIcon from '../../images/selectedOption.svg'
+import dropArrowIcon from '../../images/dropArrowIcon.svg'
 import './DropdownQuestionsMenu.css';
 import { useEffect, useRef, useState } from "react";
 import { Option } from '../../components/Option';
 
-
 interface DropdownProps {
-  title: string;
-  icon: string;
-  options: Option[];
-  onOptionClick: (option: Option) => void;
-  onOptionUnclick: (option: Option) => void;
-  selectedOptions: Option[];
+    title: string;
+    icon: string;
+    options: Option[];
+    onOptionClick: (option: Option) => void;
+    onOptionUnclick: (option: Option) => void;
+    selectedOptions: Option[];
 }
 
-export default function Dropdown({
-  title,
-  icon,
-  options,
-  onOptionClick,
-  onOptionUnclick,
-  selectedOptions,
-}: DropdownProps) {
-  const [selectedOptionIds, setSelectedOptionIds] = useState<number[]>([]);
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+export default function Dropdown({ title, icon, options, onOptionClick, onOptionUnclick, selectedOptions }: DropdownProps) {
+    const [selectedOptionIds, setSelectedOptionIds] = useState<number[]>([]);
+    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
-  //if the filter was selected, then unselect it and vice versa
-  const handleOptionClick = (option: Option) => {
-    if (selectedOptionIds.includes(option.id)) {
-      setSelectedOptionIds(selectedOptionIds.filter((id) => id !== option.id));
-      onOptionUnclick(option);
-    } else {
-      setSelectedOptionIds([...selectedOptionIds, option.id]);
-      onOptionClick(option);
-    }
-  };
-
-  //this will handle when the dropdown menu is opened, and the user clicked outside of it, so it will get closed
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setIsDropdownOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+    //if the filter was selected, then unselect it and vice versa
+    const handleOptionClick = (option: Option) => {
+        if (selectedOptionIds.includes(option.id)) {
+            setSelectedOptionIds(selectedOptionIds.filter(id => id !== option.id));
+            onOptionUnclick(option);
+        } else {
+            setSelectedOptionIds([...selectedOptionIds, option.id]);
+            onOptionClick(option);
+        }
     };
-  }, []);
 
-  //update the selected filters when a change is made
-  useEffect(() => {
-    setSelectedOptionIds(selectedOptions.map((option) => option.id));
-  }, [selectedOptions]);
+    //this will handle when the dropdown menu is opened, and the user clicked outside of it, so it will get closed
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    //update the selected filters when a change is made
+    useEffect(() => {
+        setSelectedOptionIds(selectedOptions.map(option => option.id));
+    }, [selectedOptions]);
+
     return (
         <div className="dropdown-filter" ref={dropdownRef}>
             <button className="dropbtn" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
@@ -84,7 +74,5 @@ export default function Dropdown({
                 </div>
             )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
