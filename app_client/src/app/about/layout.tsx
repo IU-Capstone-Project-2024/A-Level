72f1b8f57ea87a38b76b08b33c705eeb6b6d9103
justcgh9 <a.kabardiyadi@innopolis.university>
2024-07-18 19:23:13 +0300

    Feat. SEO

---
+++ app_client/src/app/about/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Us',
+  description: "Experience the difference with AforA, where we're dedicated to enhancing your A-level Economics revision journey. Whether you're striving to excel in exams or deepen your understanding of economics, AforA is your trusted companion, ready to assist you at every stage."
+}
+
+export default function AboutLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return children
+}
---
+++ app_client/src/app/browse/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Upload Exam',
+  description: 'Upload an existing A-level economics exam variant to the application'
+}
+
+export default function BrowseLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return children
+}
